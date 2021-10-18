487816a6f99fe7685bbacb20ce800dc03a4ffedc
Ashwaq <cs.ashwaq96@gmail.com>
2021-10-18 11:03:23 +0300

    add-load-more

---
+++ app/Http/Livewire/LoadMore.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LoadMore extends Component
+{
+    public function render()
+    {
+        return view('livewire.load-more');
+    }
+}
